69fbc33e6c84f7fecdb37d26334751935dc1faee
Ethan Smyth <es2005@hw.ac.uk>
2025-02-18 09:30:49 +0000

    Remove pending commands from UI

---
+++ Assets/QueueRemover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QueueRemover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject box = GameObject.Find("PendingCommandsBox");
+		box.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
